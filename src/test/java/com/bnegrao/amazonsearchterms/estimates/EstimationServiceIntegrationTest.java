db770f18d1946b7d5eebd3125dc550af3a945e28
Bruno Negrao G Zica <bnegrao@gmail.com>
2018-08-23 02:15:59 -0300

    remove unsused import

--- src/test/java/com/bnegrao/amazonsearchterms/estimates/EstimationServiceIntegrationTest.java
+++ src/test/java/com/bnegrao/amazonsearchterms/estimates/EstimationServiceIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.bnegrao.amazonsearchterms.estimates;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
 
 import java.util.concurrent.ExecutionException;
 
